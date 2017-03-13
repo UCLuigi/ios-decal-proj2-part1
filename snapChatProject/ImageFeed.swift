3969ecd01bbf8b9806c643470c32163450313b1d
Luis Alba <LuisAlba@airbears2-10-142-163-101.airbears2.1918.berkeley.edu>
2017-03-12 20:25:24 -0700

    Categorized images

--- snapChatProject/ImageFeed.swift
+++ snapChatProject/ImageFeed.swift
@@ -10,8 +10,8 @@ import Foundation
 import UIKit
 
 // This is the only line of code you should be modifying
-var threads: [String: [UIImage]] = ["Memes": [], "Dog Spots": [], "Random": []]
+var threads: [String: [UIImage]] = ["Memes": [#imageLiteral(resourceName: "dankMeme1"), #imageLiteral(resourceName: "dankMeme2"), #imageLiteral(resourceName: "dankMeme3"), #imageLiteral(resourceName: "dankMeme4")], "Dog Spots": [#imageLiteral(resourceName: "cutePuppy"), #imageLiteral(resourceName: "amazingCutePuppy"), #imageLiteral(resourceName: "anotherCutePuppy")], "Random": [#imageLiteral(resourceName: "berkAtNight"), #imageLiteral(resourceName: "Campanile"), #imageLiteral(resourceName: "dirks")]]
 //
 
 let threadNames = ["Memes", "Dog Spots", "Random"]
-var allImages: [UIImage] = [#imageLiteral(resourceName: "cutePuppy"), #imageLiteral(resourceName: "berkAtNight"), #imageLiteral(resourceName: "dankMeme4"), #imageLiteral(resourceName: "Campanile"), #imageLiteral(resourceName: "dankMeme1"), #imageLiteral(resourceName: "dankMeme2"), #imageLiteral(resourceName: "amazingCutePuppy"), #imageLiteral(resourceName: "cutePuppy"), #imageLiteral(resourceName: "dirks"), #imageLiteral(resourceName: "dankMeme3")]
+var allImages: [UIImage] = [#imageLiteral(resourceName: "cutePuppy"), #imageLiteral(resourceName: "berkAtNight"), #imageLiteral(resourceName: "dankMeme4"), #imageLiteral(resourceName: "Campanile"), #imageLiteral(resourceName: "dankMeme1"), #imageLiteral(resourceName: "dankMeme2"), #imageLiteral(resourceName: "amazingCutePuppy"), #imageLiteral(resourceName: "anotherCutePuppy"), #imageLiteral(resourceName: "dirks"), #imageLiteral(resourceName: "dankMeme3")]
